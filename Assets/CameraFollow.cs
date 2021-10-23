056a380eb009cc865dc1e241f1e1319d45ae9ca0
iapk <iapk2727@gmail.com>
2021-10-23 11:45:04 -0500

    Balls.A.034
    -Started camera alt look
    -Stairs prefab added

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -3,13 +3,20 @@
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
+    //Camera cam;
+    //public Vector3 mp;
 
     public float smoothSpeed = 0.125f; //put between 0 and 1 small is smoother
     public Vector3 offset; //Pulls camera away from character
 
+
+
     private void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset; //Exact camera follow
+
+        //mp = cam.ScreenToWorldPoint(Input.mousePosition);
+        //mp.z = 15;
+        Vector3 desiredPosition = (target.position + offset); //Exact camera follow   - mp
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed); //Smoothed camera adjustment
         transform.position = smoothedPosition; //Applying camera position
     }
