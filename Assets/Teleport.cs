355810ee5697541b205187375af4f04c0459e4ad
iapk <iapk2727@gmail.com>
2021-10-26 01:09:31 -0500

    Balls.A.043
    -Added teleport code
    -Added Audio manager
    -Added Switch Music Trigger
    -Art overhaul
    -Final stage music
    -Credits
    -Lamp adjust
    -Kitchen adjust
    -Roof under adjust

---
+++ Assets/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform destination;
+    public GameObject thePlayer;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            thePlayer.transform.position = destination.transform.position;
+        }
+    }
+}
